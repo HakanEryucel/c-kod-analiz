4d945045258048ccebbe60736332d5c855a657c0
Hakan Eryücel <HakanEryucel@users.noreply.github.com>
2019-02-26 13:39:07 +0300

    Düzeltme

--- src/pdpodev/PDPOdev.java
+++ src/pdpodev/PDPOdev.java
@@ -9,6 +9,7 @@ public class PDPOdev
     {
         int sayi = 0;
         boolean tirnak=false;
+        boolean cokluYorum=false;
         BufferedReader reader = new BufferedReader(new FileReader(dosya));
         String satir;
         satir=reader.readLine();
@@ -16,20 +17,25 @@ public class PDPOdev
         {
         for(int i=0;i<satir.length();i++)
         {
+            if(satir.charAt(i)=='/'&&satir.charAt(i+1)=='/')
+            {
+                break;
+            }
+            if(satir.charAt(i)=='/'&&satir.charAt(i+1)=='*')
+            {
+                cokluYorum=true;
+            }
+            if(satir.charAt(i)=='*'&&satir.charAt(i+1)=='/')
+            {
+                cokluYorum=false;
+            }
             if(satir.charAt(i)=='"')
             {
-                if(tirnak)
-                {
-                    tirnak=false;
-                }
-                else
-                {
-                    tirnak=true;
-                }
+                tirnak = !tirnak;
             }
-            if(!tirnak)
+            if(!tirnak&&!cokluYorum)
             {
-                if((satir.charAt(i)=='+'&&satir.charAt(i+1)!='+')||(satir.charAt(i)=='-'&&satir.charAt(i+1)!='-')||satir.charAt(i)=='/'||satir.charAt(i)=='*'||(satir.charAt(i)=='&'&&satir.charAt(i+1)!='&')||(satir.charAt(i)=='='&&satir.charAt(i+1)!='=')||satir.charAt(i)=='<'||satir.charAt(i)=='>'||(satir.charAt(i)=='!'&&satir.charAt(i+1)=='=')||(satir.charAt(i)=='|'&&satir.charAt(i+1)=='|')) 
+                if((satir.charAt(i)=='+'&&satir.charAt(i+1)!='+')||(satir.charAt(i)=='-'&&satir.charAt(i+1)!='-')||(satir.charAt(i)=='/'&&satir.charAt(i+1)!='/'&&satir.charAt(i-1)!='/'&&satir.charAt(i+1)!='*')||(satir.charAt(i)=='*'&&satir.charAt(i-1)!='/')||(satir.charAt(i)=='&'&&satir.charAt(i+1)!='&')||(satir.charAt(i)=='='&&satir.charAt(i+1)!='=')||satir.charAt(i)=='<'||satir.charAt(i)=='>'||(satir.charAt(i)=='!'&&satir.charAt(i+1)=='=')||(satir.charAt(i)=='|'&&satir.charAt(i+1)=='|')) 
                 {// +, -, /, *, &, ++, --, +=, -=, /=, *=, =, ==, <, >, <=, >=, !=.  &&, || 
                     sayi++;
                 }
@@ -42,27 +48,121 @@ public class PDPOdev
     
     static boolean fonksiyonMu(String satir)
     {
+        boolean anahtar=false,parantez=false,esittir=false;
+        int tmp=0;
+        if(satir.contains("void")||satir.contains("char")||satir.contains("int")||satir.contains("short")||satir.contains("long")||satir.contains("float")||satir.contains("double"))
+        {
+            anahtar=true;
+        }
+        if(satir.contains("(")&&satir.contains(")"))
+        {
+            parantez=true;
+        }
+        for(int i=0;i<satir.length();i++)
+        {
+            if(satir.charAt(i)=='(')
+            {
+                tmp=i;
+                break;
+            }
+        }
+        if(tmp!=0)
+        {
+            satir=satir.substring(1, tmp-1);
+        }
+        if(satir.contains("="))
+        {
+            esittir=true;
+        }
+        return (anahtar&&parantez)&&(!esittir);
     }
     static int fonksiyonSay(File dosya)throws IOException
     {
         int sayi=0;
-        
+        BufferedReader reader = new BufferedReader(new FileReader(dosya));
+        String satir=reader.readLine();
+        while(satir!=null)
+        {
+            /*if(satir.contains("void")||satir.contains("return"))
+            {
+                sayi++;
+            }
+            */
+            if(fonksiyonMu(satir))
+            {
+                sayi++;
+            }
+            satir=reader.readLine();
+        }
         return sayi;
+    }
+    static void deneme(File dosya)throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new FileReader(dosya));
+        String metin="";
+        int karakter;
+        while((karakter= reader.read())!=-1)
+        {
+            char tmp=(char)karakter;
+            if(tmp!='\n'&&tmp!='\r')
+            {
+                metin+=tmp;
+            }
+            else
+            {
+                metin+=' ';
+            }
+        }
+        System.out.println(metin);
     }
     static int parametreSay(File dosya)throws IOException
     {
         int sayi=0;
-        
+        BufferedReader reader = new BufferedReader(new FileReader(dosya));
+        String satir=reader.readLine();
+        while(satir!=null)
+        {
+            if(satir.contains("void"))
+            {
+                int ilk=0,son=0;
+                for(int i=0;i<satir.length();i++)
+                {
+                    if(satir.charAt(i)=='(')
+                    {
+                        ilk=i;
+                    }
+                    if(satir.charAt(i)==')')
+                    {
+                        son=i;
+                    }
+                }
+                if(son-ilk!=1)
+                {
+                    satir=satir.substring(ilk+1, son);
+                    int tmp=0;
+                    for(int i=0;i<satir.length();i++)
+                    {
+                        if(satir.charAt(i)==',')
+                        {
+                            tmp++;
+                        }
+                    }
+                    sayi+=tmp+1;
+                }
+            }
+            satir=reader.readLine();
+        }
         return sayi;
     }
     public static void main(String[] args) throws IOException 
     {
         File dosya = new File("Program.c");
         int operatorSayi=operatorSay(dosya);
-        int fonksiyonSayi=fonksiyonSay(dosya);
-        int parametreSayi=parametreSay(dosya);
+        /*int fonksiyonSayi=fonksiyonSay(dosya);
+        int parametreSayi=parametreSay(dosya);*/
         System.out.println("Toplam Operatör Sayısı: "+operatorSayi);
-        System.out.println("Toplam Fonksiyon Sayısı: "+fonksiyonSayi);
-        System.out.println("Toplam Parametre Sayısı: "+parametreSayi);
+        deneme(dosya);
+       /* System.out.println("Toplam Fonksiyon Sayısı: "+fonksiyonSayi);
+        System.out.println("Toplam Parametre Sayısı: "+parametreSayi);*/
     }
 }
