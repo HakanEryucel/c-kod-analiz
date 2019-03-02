42d2f04a9800165cd7a413e109d59f904527cdec
Hakan Eryücel <HakanEryucel@users.noreply.github.com>
2019-03-02 22:12:13 +0300

    Proje Tamamlandı.

--- Program.c
+++ Program.c
@@ -2,45 +2,34 @@
 #include "stdlib.h" 
  
 void DiziYazdir(int *p,int uzunluk)
-{  
-	int i=0;  
+{
+	int i=0;
 	for(;i<uzunluk;i++)
-		printf("%d ",p[i]); 
+		printf("%d ",p[i]);
 }
-int DiziTopla(int *p,int uzunluk)  
-{  
-	int i=0;  
-	int toplam=0;    
-	for(;i<uzunluk;i++) 
-		toplam += p[i];    
-	return toplam; 
-} // +, -, /, *, &, ++, --, +=, -=, /=, *=, =, ==, <, >, <=, >=, !=.  &&, || 
-/**/
-
+int DiziTopla(int *p,int uzunluk)
+{
+	int i=0;
+	int toplam=0;
+	for(;i<uzunluk;i++)
+		toplam += p[i];
+	return toplam;
+}
 int main()
-{  
-	int x = 3+5;
-	int y = 9 - 6;
-	int c = 8 / 4 ;
-	int x = 5   *   5 ;
-	int y = 5++;/**/
-	if( a &);
-	int x=        c++;
-	int c=   x-- ;
-	if ( += -= /= *=  =  ==  <  > <=  >= != && ||);
-	int x,y;  
-	printf("x:");  
-	scanf("%d",&x);  
-	printf("y:");  
-	scanf("%d",&y);  
-	int sonuc = x+y;  
-	printf("Sonuc:%d\n\n",sonuc);  
-	int *dizi = malloc(3*sizeof(int));  
-	dizi[0]=x;  
-	dizi[1]=y;  
-	dizi[2]=sonuc;  
-	DiziYazdir(dizi,3);  
-	printf("\n\nToplam Deger:%d",DiziTopla(dizi,3));  
-	free(dizi);  
-	return 0; 
+{
+	int x,y;
+	printf("x:");
+	scanf("%d",&x);
+	printf("y:");
+	scanf("%d",&y);
+	int sonuc = x+y;
+	printf("Sonuc:%d\n\n",sonuc);
+	int *dizi = malloc(3*sizeof(int));
+	dizi[0]=x;
+	dizi[1]=y;
+	dizi[2]=sonuc;
+	DiziYazdir(dizi,3);
+	printf("\n\nToplam Deger:%d",DiziTopla(dizi,3));
+	free(dizi);
+	return 0;
 }
--- src/pdpodev/PDPOdev.java
+++ src/pdpodev/PDPOdev.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 public class PDPOdev 
 {
-    static int operatorSay(File dosya)throws IOException
+    static int operatorSay(File dosya)throws IOException//Operatorleri sayıyor.
     {
         int sayi = 0;
         boolean tirnak=false;
@@ -17,27 +17,30 @@ public class PDPOdev
         {
         for(int i=0;i<satir.length()-1;i++)
         {
-            if(satir.charAt(i)=='/'&&satir.charAt(i+1)=='/')
+            if(i!=satir.length()-1)
             {
-                break;
-            }
-            if(satir.charAt(i)=='/'&&satir.charAt(i+1)=='*')
-            {
-                cokluYorum=true;
-            }
-            if(satir.charAt(i)=='*'&&satir.charAt(i+1)=='/')
-            {
-                cokluYorum=false;
-            }
-            if(satir.charAt(i)=='"')
-            {
-                tirnak = !tirnak;
-            }
-            if(!tirnak&&!cokluYorum)
-            {
-                if((satir.charAt(i)=='+'&&satir.charAt(i+1)!='+')||(satir.charAt(i)=='-'&&satir.charAt(i+1)!='-')||(satir.charAt(i)=='/'&&satir.charAt(i+1)!='*'&&satir.charAt(i-1)!='*')||(satir.charAt(i)=='*'&&satir.charAt(i-1)!='/'&&satir.charAt(i+1)!='/')||(satir.charAt(i)=='&'&&satir.charAt(i+1)!='&')||(satir.charAt(i)=='='&&satir.charAt(i+1)!='=')||(satir.charAt(i)=='<'&&satir.charAt(i+1)!='=')||(satir.charAt(i)=='>'&&satir.charAt(i+1)!='=')||(satir.charAt(i)=='|'&&satir.charAt(i+1)=='|')) 
-                {// +, -, /, *, &, ++, --, +=, -=, /=, *=, =, ==, <, >, <=, >=, !=.  &&, || 
-                    sayi++;
+                if(satir.charAt(i)=='/'&&satir.charAt(i+1)=='/')//Yorum satiri geldiyse siradaki satira geçmesini sağlıyor.
+                {
+                    break;
+                }
+                if(satir.charAt(i)=='/'&&satir.charAt(i+1)=='*')// Çoklu  yorum açıldıysa kapatılana kadar işlem yapmıyor.
+                {
+                    cokluYorum=true;
+                }
+                if(satir.charAt(i)=='*'&&satir.charAt(i+1)=='/')//Çoklu yorum bittiğinde operatorleri saymaya devam ediyor.
+                {
+                    cokluYorum=false;
+                }
+                if(satir.charAt(i)=='"'||satir.charAt(i)=='\'')//Tirnak açıldıysa operator saymayı durduruyor. 
+                {
+                    tirnak = !tirnak;
+                }
+                if(!tirnak&&!cokluYorum)//Koşullar sağlandıysa operator sayıyor.
+                {
+                    if((satir.charAt(i)=='+'&&satir.charAt(i+1)!='+')||(satir.charAt(i)=='-'&&satir.charAt(i+1)!='-')||(satir.charAt(i)=='/'&&satir.charAt(i+1)!='*'&&(i!=0&&satir.charAt(i-1)!='*'))||(satir.charAt(i)=='*'&&i!=0&&satir.charAt(i-1)!='/'&&satir.charAt(i+1)!='/')||(satir.charAt(i)=='&'&&satir.charAt(i+1)!='&')||(satir.charAt(i)=='='&&satir.charAt(i+1)!='=')||(satir.charAt(i)=='<'&&satir.charAt(i+1)!='=')||(satir.charAt(i)=='>'&&satir.charAt(i+1)!='=')||(satir.charAt(i)=='|'&&satir.charAt(i+1)=='|')) 
+                    {// +, -, /, *, &, ++, --, +=, -=, /=, *=, =, ==, <, >, <=, >=, !=.  &&, || 
+                        sayi++;
+                    }
                 }
             }
         }
@@ -45,7 +48,7 @@ public class PDPOdev
         }
         return sayi;
     }
-    static String[] sablonDonustur(File dosya)throws IOException
+    static String[] diziDonustur(File dosya)throws IOException//SablonDonustur fonksiyonu sadece fonksiyonların adı ve parametreleri bulunan string bir dizi döndürüyor.
     {
         int suslu=0;
         boolean tirnak=false;
@@ -57,112 +60,149 @@ public class PDPOdev
         {
         for(int i=0;i<satir.length();i++)
         {
-            if(satir.charAt(i)=='{')
+            if(i!=satir.length()-1)// Eğer satırın son karakteri değil ise bu kısımdaki işlemler yapılıyor. Sonraki karaktere ulaşılabiliyor.
             {
-                suslu++;
-                if(suslu==1)
+                if(satir.charAt(i)=='{')//Açılan her süsşü parantez için suslu değişkenini bir arttırıyor.Süslü parantezlerin içini aktarmayacagımız için döngüden çıkıyor.
                 {
-                    //metin+='{';
-                    break;
-                }
-            }
-            if(satir.charAt(i)=='}')
-            {
-                suslu--;
-            }
-            if((satir.charAt(i)=='/'&&satir.charAt(i+1)=='/')||suslu>0||satir.charAt(i)=='#')
-            {
-                break;
-            }
-            if(satir.charAt(i)=='/'&&satir.charAt(i+1)=='*')
-            {
-                cokluYorum=true;
-            }
-            if(satir.charAt(i)=='*'&&satir.charAt(i+1)=='/')
-            {
-                cokluYorum=false;
-            }
-            if(satir.charAt(i)=='"')
-            {
-                tirnak = !tirnak;
-            }
-            if(!tirnak&&!cokluYorum)
-            {
-                if(satir.charAt(i)!='\n'&&satir.charAt(i)!='"'&&satir.charAt(i)!=' '&&satir.charAt(i)!='\r'&&satir.charAt(i)!='\t'&&satir.charAt(i)!='(')
-                {
-                    metin+=satir.charAt(i);
-                }
-                else if(satir.charAt(i)=='\n'||satir.charAt(i)=='\r'||satir.charAt(i)=='\t'||satir.charAt(i)==' ')
-                {
-                    if(!metin.endsWith(" "))
+                    suslu++;
+                    if(suslu==1)
                     {
-                        metin+=' ';
+                        break;
                     }
                 }
-                else if(satir.charAt(i)=='(')
+                if(satir.charAt(i)=='}')//Süslü parantez kapatıldığında süslü değişkenini bir azaltıyor.
                 {
-                    metin=metin.trim();
-                    metin+='(';
+                    suslu--;
+                }
+                if((satir.charAt(i)=='/'&&satir.charAt(i+1)=='/')||suslu>0||satir.charAt(i)=='#')// Yorum satırı veya önişlemci komutu var ise diziye aktarmayacağımız için döngüden çıkıyor.
+                {
+                    break;
+                }
+                
+                if(satir.charAt(i)=='/'&&satir.charAt(i+1)=='*')//Çoklu yorum açıldığında kapatılana kadar işlemleri durduruyor.
+                {
+                    cokluYorum=true;
+                }
+                if(satir.charAt(i)=='*'&&satir.charAt(i+1)=='/')//Süslü parantez kapatıldığında işlemlerin devam etmesini sağlıyor.
+                {
+                    cokluYorum=false;
+                }
+                if(satir.charAt(i)=='"')//Tırnak açıldığında kapatılana kadar işlemleri durduruyor.
+                {
+                    tirnak = !tirnak;
+                }
+                if(!tirnak&&!cokluYorum)//Koşullar sağlanıyor ise işlemler yapılıyor.
+                {
+                    //Eğer gelen karakter önemsiz bir karakter değil ise metin değişkenimize ekliyor.
+                    if(satir.charAt(i)!='\n'&&satir.charAt(i)!='"'&&satir.charAt(i)!=' '&&satir.charAt(i)!='\r'&&satir.charAt(i)!='\t'&&satir.charAt(i)!='(')
+                    {
+                        metin+=satir.charAt(i);
+                    }
+                    //Metin değişkeni içinde arka arkaya boşluk olmaması için ve tek satır halinde olması için birden fazla boşlukları ve satır sonu karakterini siliyor. Yerine boşluk yazıyor.
+                    else if(satir.charAt(i)=='\n'||satir.charAt(i)=='\r'||satir.charAt(i)=='\t'||satir.charAt(i)==' ')
+                    {
+                        if(!metin.endsWith(" "))
+                        {
+                            metin+=' ';
+                        }
+                    }
+                    else if(satir.charAt(i)=='(')//Parantezden önce boşluk olmaması için parantez geldiğinde metin stringinin sonundaki boşlukları siliyor.
+                    {
+                        metin=metin.trim();
+                        metin+='(';
+                    }
+                }
+            }
+            else// Yukarıdaki işlemlerin aynısı yapılıyor fakat satırın karakterine gelindiği için bir sonraki karaktere erişmeye çalışması engelleniyor.
+            {
+                if(satir.charAt(i)=='{')
+                {
+                    suslu++;
+                    if(suslu==1)
+                    {
+                        //metin+='{';
+                        break;
+                    }
+                }
+                if(satir.charAt(i)=='}')
+                {
+                    suslu--;
+                }
+                if(suslu>0||satir.charAt(i)=='#')
+                {
+                    break;
+                }
+                if(satir.charAt(i)=='"')
+                {
+                    tirnak = !tirnak;
+                }
+                if(!tirnak&&!cokluYorum)
+                {
+                    if(satir.charAt(i)!='\n'&&satir.charAt(i)!='"'&&satir.charAt(i)!=' '&&satir.charAt(i)!='\r'&&satir.charAt(i)!='\t'&&satir.charAt(i)!='(')
+                    {
+                        metin+=satir.charAt(i);
+                    }
+                    else if(satir.charAt(i)=='\n'||satir.charAt(i)=='\r'||satir.charAt(i)=='\t'||satir.charAt(i)==' ')
+                    {
+                        if(!metin.endsWith(" "))
+                        {
+                            metin+=' ';
+                        }
+                    }
+                    else if(satir.charAt(i)=='(')
+                    {
+                        metin=metin.trim();
+                        metin+='(';
+                    }
                 }
             }
         }
         satir=reader.readLine();
         }
-        String [] fonksiyonDizi=metin.split("}");
+        String [] fonksiyonDizi=metin.split("}");//Metinimizin içindeki süslü parantezlere göre metini bölüyoruz.Süslü parantezden öncesi fonksiyon isimlerini ve parametrelerini barındıran kısım oluyor.
         return fonksiyonDizi;
     }
-    static int fonksiyonSay(File dosya)throws IOException
+    static int fonksiyonSay(File dosya)throws IOException//Sablon dönüştür sadece fonksiyon adlarından oluşan bir dizi döndürüyor, bu dizinin uzunluğu fonksiyon sayısını veriyor.
     {
-        String []fonksiyonDizi=sablonDonustur(dosya);
-        //int ayni=0;
-       // for(int i=0;i<fonksiyonDizi.length;i++)
-       // {
-           // for(int j=0;j<fonksiyonDizi.length;j++)
-           // {
-             //   if(i!=j&&fonksiyonDizi[i].equals(fonksiyonDizi[j]))
-              //  {
-              //      ayni++;
-               // }
-           // }
-        //}
-        return fonksiyonDizi.length;//-ayni;
+        String []fonksiyonDizi=diziDonustur(dosya);
+        return fonksiyonDizi.length;
     }
-    static int parametreSay(File dosya)throws IOException
+    static int parametreSay(File dosya)throws IOException//SablonDonustur den dönen dizideki her string elemanın parantezler arasındaki paremetre sayısını buluyor.
     {
         int sayi=0;
-        String[] fonksiyonDizi=sablonDonustur(dosya);
+        String[] fonksiyonDizi=diziDonustur(dosya);
         for (String tmp : fonksiyonDizi) {
             boolean parantez=false;
             for (int j = 0; j < tmp.length(); j++) {
                 if (tmp.charAt(j) == '(' && !parantez) {
                     parantez=true;
-                    if (tmp.length() - j <= 2) {
+                    if (tmp.length() - j <= 2) {//Boş ise çıkıyor.
                         break;
                     } else {
-                        sayi++;
+                        sayi++;//Boş değilse kesinlikle en az bir tane olduğu için bir arttırıyor ve sonra her virgül sayısı kadar arttıracak.
                     }
                 }
-                if (parantez && tmp.charAt(j) == ',') {
+                if (parantez && tmp.charAt(j) == ',') {//Virgül sayisi kadar arttırıyor.
                     sayi++;
                 }
             }
         }
         return sayi;
     }
-    static void fonksiyonYazdir(File dosya)throws IOException
+    static void yazdir(File dosya)throws IOException //sablonDonustur den gelen dizinin her string elemanını fonksiyon adi ve paremetrelerine bölüp yazdırıyor.
     {
         System.out.println("Fonksiyon İsimleri:");
-        String[] fonksiyonDizi=sablonDonustur(dosya);
+        String[] fonksiyonDizi=diziDonustur(dosya);
         for(String tmp : fonksiyonDizi)
         {
             int boslukIndex=0,ilkParantezIndex=0,ikinciParantezIndex=0;
             for(int i=0;i<tmp.length();i++)
             {
-                if(tmp.charAt(i)==' '&&ilkParantezIndex==0)
+                if(tmp.charAt(i)==' '&&ilkParantezIndex==0)//Parantezlerden önceki son boşluğun indexini buluyor. Bu indeksten parantezlere kadar olan kısım fonksiyon adı olmuş oluyor.
                 {
                     boslukIndex=i;
                 }
-                if(tmp.charAt(i)=='(')
+                if(tmp.charAt(i)=='(')//Parantezlerin indexlerini bulup arasındakilerden parametreleri buluyor.
                 {
                     ilkParantezIndex=i;
                 }
@@ -172,11 +212,11 @@ public class PDPOdev
                     break;
                 }
             }
-            System.out.print(tmp.substring(boslukIndex+1, ilkParantezIndex).trim()+" - Parametreler: ");
-            String[] parametreDizi=tmp.substring(ilkParantezIndex+1,ikinciParantezIndex).split(",");
+            System.out.print(tmp.substring(boslukIndex+1, ilkParantezIndex).trim()+" - Parametreler: ");//Boşluk index ve parantez arasındakini yazdiriyor.Fonksiyon adi yazilmiş oluyor.
+            String[] parametreDizi=tmp.substring(ilkParantezIndex+1,ikinciParantezIndex).split(",");//İki parantez arasını virgüllere göre ayırıp diziye atıyor.
             for(String tmp2:parametreDizi)
             {
-                String[] parametre=tmp2.split(" ");
+                String[] parametre=tmp2.split(" ");//Parametre dizisini yazdırıyor.
                 System.out.print(parametre[parametre.length-1]);
                 if(!tmp2.equals(parametreDizi[parametreDizi.length-1]))
                 {
@@ -189,9 +229,9 @@ public class PDPOdev
     public static void main(String[] args) throws IOException 
     {
         File dosya = new File("Program.c");
-        System.out.println("Toplam Operatör Sayısı: "+operatorSay(dosya));
-        System.out.println("Toplam Fonksiyon Sayısı: "+fonksiyonSay(dosya));
-        System.out.println("Toplam Parametre Sayısı: "+parametreSay(dosya));
-        fonksiyonYazdir(dosya);
+        System.out.println("Toplam Operatör Sayısı: "+operatorSay(dosya));//Operator sayısını yazdırıyor.
+        System.out.println("Toplam Fonksiyon Sayısı: "+fonksiyonSay(dosya));//Fonksiyon sayisini yazdiriyor.
+        System.out.println("Toplam Parametre Sayısı: "+parametreSay(dosya));// Parametre sayısını yazdırıyor.
+        yazdir(dosya);//Fonksiyonları ve parametreleri yazdırıyor.
     }
 }
